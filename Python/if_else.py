5ec6a5453a26bf721dffb2bc1eb4c035441519dd
kishcods <97351332+kishcods@users.noreply.github.com>
2022-01-13 22:21:25 +0530

    Update if_else.py

--- Python/if_else.py
+++ Python/if_else.py
@@ -1,21 +1,20 @@
-"""
-Task
-Given an integer, , perform the following conditional actions:
+# Python If-Else
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-Input Format
+# Task 
+# Given an integer, n, perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
 
-A single line containing a positive integer, .
+# Input Format
+# A single line containing a positive integer, n.
 
-Constraints
+# Constraints
+# 1 <= n <= 100
 
-Output Format
-
-Print Weird if the number is weird. Otherwise, print Not Weird.
-"""
+# Output Format
+# Print Weird if the number is weird; otherwise, print Not Weird.
 
 if __name__ == '__main__':
     n = int(raw_input().strip())
